d05e6df3eeb5fecfc3c1c4cc8efdebede36f1ee1
mankae <mbk.kaelin@gmail.com>
2026-07-01 11:35:07 +0200

    Fix import statement for STES in __init__.py and update __all__ list

--- src/stes_tools/__init__.py
+++ src/stes_tools/__init__.py
@@ -1,7 +1,7 @@
 from .H2O_prop import density_water, specific_heat_water
 from .cost_functions import CAPEX_STES, OPEX_STES
 from .import_functions import data_import, PTES_geometry_import, extract_storage_temperature
-from .loss_simulation import PTES, TTES
+from .loss_simulation import STES, PTES, TTES
 from .temperature_map import *
 
 __all__ = [
@@ -12,6 +12,7 @@ __all__ = [
     "data_import",
     "PTES_geometry_import",
     "extract_storage_temperature",
+    "STES",
     "PTES",
     "TTES",
     "temperature_map"
