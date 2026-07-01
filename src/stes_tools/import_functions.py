d9c69da36a72c36d4c11d48c4cedcb77125c79be
mankae <mbk.kaelin@gmail.com>
2026-07-01 11:56:29 +0200

    Refactor data_import function documentation and comment out PTES_geometry_import

--- src/stes_tools/import_functions.py
+++ src/stes_tools/import_functions.py
@@ -4,23 +4,14 @@ import numpy as np
 def data_import(file_path):
     '''Imports data from an Excel file and returns a DataFrame
 
-    Structure of the Excel file:
-
-    Sheet 1: 'data' contains the following columns:
+    Excel file has to contain a sheet called 'data' with the following columns:
     - time: Date and Time
     - Q_storage (NEEDED FOR VALIDATION. FOR SIMULATION ONLY THE INITIAL VALUE IS NEEDED): Energy content of the storage (MWh)
     - Q_charge: Energy charged into the storage (MWh)
     - Q_discharge: Energy discharged from the storage (MWh)
     - T_air: Air temperature at the storage location (°C)
     - T_soil: Soil temperature at the storage location (°C)
-    - T_XX (NEEDED FOR VALIDATION. FOR SIMULATION TEMPERATURE MAP IS USED): Temperature at different heights (increasing from bottom to top XX = 01, 02, ...) in the storage (°C)
-    
-    Sheet 2: 'geometry' contains the following columns:
-    - h: Height of the truncated pyramid (m)
-    - a: Length of the upper rectangle (m)
-    - b: Width of the upper rectangle (m)
-    - c: Length of the lower rectangle (m)
-    - d: Width of the lower rectangle (m)'''
+    - T_XX (NEEDED FOR VALIDATION. FOR SIMULATION TEMPERATURE MAP IS USED): Temperature at different heights (increasing from bottom to top XX = 01, 02, ...) in the storage (°C)'''
 
     # data import
     df_data = pd.read_excel(file_path, sheet_name='data')
@@ -37,33 +28,22 @@ def extract_storage_temperature(data):
 
     return T_storage
 
-def PTES_geometry_import(file_path):
-    '''Imports geometry data from an Excel file and returns the parameters
+# def PTES_geometry_import(file_path):
+#     '''Imports geometry data from an Excel file and returns the parameters
     
-    Structure of the Excel file:
+#     Excel file has to contain a sheet called 'geometry' with the following columns:
+#     - h: Height of the truncated pyramid (m)
+#     - a: Length of the upper rectangle (m)
+#     - b: Width of the upper rectangle (m)
+#     - c: Length of the lower rectangle (m)
+#     - d: Width of the lower rectangle (m)'''
 
-    Sheet 1: 'data' contains the following columns:
-    - time: Date and Time
-    - Q_storage (NEEDED FOR VALIDATION. FOR SIMULATION ONLY THE INITIAL VALUE IS NEEDED): Energy content of the storage (MWh)
-    - Q_charge: Energy charged into the storage (MWh)
-    - Q_discharge: Energy discharged from the storage (MWh)
-    - T_air: Air temperature at the storage location (°C)
-    - T_soil: Soil temperature at the storage location (°C)
-    - T_XX (NEEDED FOR VALIDATION. FOR SIMULATION TEMPERATURE MAP IS USED): Temperature at different heights (increasing from bottom to top XX = 01, 02, ...) in the storage (°C)
-    
-    Sheet 2: 'geometry' contains the following columns:
-    - h: Height of the truncated pyramid (m)
-    - a: Length of the upper rectangle (m)
-    - b: Width of the upper rectangle (m)
-    - c: Length of the lower rectangle (m)
-    - d: Width of the lower rectangle (m)'''
+#     # geometry import
+#     df_geometry = pd.read_excel(file_path, sheet_name='geometry')
+#     h = df_geometry['h'][0]
+#     a = df_geometry['a'][0]
+#     b = df_geometry['b'][0]
+#     c = df_geometry['c'][0]
+#     d = df_geometry['d'][0]
 
-    # geometry import
-    df_geometry = pd.read_excel(file_path, sheet_name='geometry')
-    h = df_geometry['h'][0]
-    a = df_geometry['a'][0]
-    b = df_geometry['b'][0]
-    c = df_geometry['c'][0]
-    d = df_geometry['d'][0]
-
-    return h, a, b, c, d
+#     return h, a, b, c, d
